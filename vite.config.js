fa94256120a9bee8b618411234d06b21152cd69e
Alina Akchurina <ohhalina13@gmail.com>
2025-06-17 22:14:00 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tap-tapoica/",
   plugins: [react()],
-  base: "/tap-tapoica/"
 })
 
 
